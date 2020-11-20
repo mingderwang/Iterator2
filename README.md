54d98cef0e08dae86f37bc7db178e38513a85e0f
Ming-der Wang <mingder78@yahoo.com>
2020-11-20 13:20:09 +0000

    iter from objects

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python Design Pattern - Object Pool
+[credit](https://sourcemaking.com/design_patterns/object_pool)
+
+# UML Class Diagram
+![](https://sourcemaking.com/files/v2/content/patterns/Iterator.png)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Python 很多物件已經用了 Iterator pattern
+# Python 很多類別已經用了 Iterator pattern, 例如 collection, sequence (such as: string, list, tuple), non-sequence (such as: dict, file object, objects of any class with __iter__() or __getitem__() impletmented)
 mytuple = ("apple", "banana", "cherry")
 
 for i in mytuple:
@@ -15,9 +15,10 @@ print(next(myit))
 class MyNumbers:
   def __iter__(self):
     self.a = 1
+    print(type(self))
     return self
 
-  def __next__(self):
+  def __next__(self) -> int:
     x = self.a
     self.a += 1
     return x
@@ -30,3 +31,71 @@ print(next(myiter))
 print(next(myiter))
 print(next(myiter))
 print(next(myiter))
+
+
+# list
+# define a list
+print('--- list 也是 iterator ---')
+my_list = [4, 7, 0, 3]
+
+# get an iterator using iter()
+my_iter = iter(my_list)
+
+# iterate through it using next()
+
+# Output: 4
+print(next(my_iter))
+
+# Output: 7
+print(next(my_iter))
+print('---------- list them all ---')
+
+# iterable  
+# create an iterator object from that iterable
+iter_obj = iter(my_iter)
+
+# infinite loop
+while True:
+    try:
+        # get the next item
+        element = next(iter_obj)
+        # do something with element
+        print(element)
+    except StopIteration:
+        # if StopIteration is raised, break from loop
+        break
+
+
+# class example
+class PowTwo:
+    """Class to implement an iterator
+    of powers of two"""
+
+    def __init__(self, max=0):
+        self.max = max
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n <= self.max:
+            result = 2 ** self.n
+            self.n += 1
+            return result
+        else:
+            raise StopIteration
+
+
+# create an object
+numbers = PowTwo(3)
+
+# create an iterable from the object
+i = iter(numbers)
+
+# Using next to get to the next iterator element
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
